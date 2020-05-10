ca79d5643b4e5f637cf960940beb516aa86c6880
lopes <lopes@arquia.com>
2020-05-10 18:25:09 -0300

    adding one more validation

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -15,6 +15,7 @@ class CreateTransactionService {
   }
 
   public execute({ title, value, type }: Request): Transaction {
+    if (!['income', 'outcome'].includes(type)) throw new Error('Tipo inválido');
     const { total } = this.transactionsRepository.getBalance();
 
     if (type === 'outcome' && total < value)
